6aefe9a02c91fe4837d74c3661a406c8f963cf1f
Nick <njlm65@mun/ca>
2017-09-28 17:14:54 -0230

    second commit

---
+++ Notes.md
@@ -0,0 +1,5 @@
+Reducer
+is a function that reutrns the piece of the applications state
+
+Container
+react component connected to the state in redux 
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Javascript the good parts'},
+    {title: 'Harry'},
+    {title: 'something'},
+    {title: 'something 2'}
+  ]
+}
